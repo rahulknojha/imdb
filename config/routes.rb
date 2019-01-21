63f36cac2a8368ec577e37915ba020ea5db7ad4c
Ankitap123 <ankitapandey141296@gmail.com>
2019-01-21 17:28:48 +0530

    Tv CRUD and devise-gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+#Tv_Routes
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   devise_for :users
+   root to: 'tvs#index'
+
+   resources :tvs
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,98 +10,104 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_117_072_500) do
-  create_table 'awards', force: :cascade do |t|
-    t.string 'award_name'
-    t.string 'category'
-    t.string 'awardable_type'
-    t.integer 'awardable_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[awardable_type awardable_id], name: 'index_awards_on_awardable_type_and_awardable_id'
+ActiveRecord::Schema.define(version: 2019_01_21_075233) do
+
+  create_table "awards", force: :cascade do |t|
+    t.string "award_name"
+    t.string "category"
+    t.string "awardable_type"
+    t.integer "awardable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["awardable_type", "awardable_id"], name: "index_awards_on_awardable_type_and_awardable_id"
   end
 
-  create_table 'films_personnels', id: false, force: :cascade do |t|
-    t.integer 'film_id', null: false
-    t.integer 'personnel_id', null: false
-    t.index %w[film_id personnel_id], name: 'index_films_personnels_on_film_id_and_personnel_id'
-    t.index %w[personnel_id film_id], name: 'index_films_personnels_on_personnel_id_and_film_id'
+  create_table "films_personnels", id: false, force: :cascade do |t|
+    t.integer "film_id", null: false
+    t.integer "personnel_id", null: false
+    t.index ["film_id", "personnel_id"], name: "index_films_personnels_on_film_id_and_personnel_id"
+    t.index ["personnel_id", "film_id"], name: "index_films_personnels_on_personnel_id_and_film_id"
   end
 
-  create_table 'genres', force: :cascade do |t|
-    t.string 'name'
-    t.string 'genreable_type'
-    t.integer 'genreable_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[genreable_type genreable_id], name: 'index_genres_on_genreable_type_and_genreable_id'
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.string "genreable_type"
+    t.integer "genreable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["genreable_type", "genreable_id"], name: "index_genres_on_genreable_type_and_genreable_id"
   end
 
-  create_table 'movies', force: :cascade do |t|
-    t.string 'title'
-    t.string 'language'
-    t.datetime 'release_date'
-    t.decimal 'collection'
-    t.decimal 'budget'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "language"
+    t.datetime "release_date"
+    t.decimal "collection"
+    t.decimal "budget"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'personnels', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "personnels", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'reviews', force: :cascade do |t|
-    t.integer 'rating'
-    t.string 'review'
-    t.datetime 'datetime'
-    t.string 'reviewable_type'
-    t.integer 'reviewable_id'
-    t.integer 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[reviewable_type reviewable_id], name: 'index_reviews_on_reviewable_type_and_reviewable_id'
-    t.index ['user_id'], name: 'index_reviews_on_user_id'
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "review"
+    t.datetime "datetime"
+    t.string "reviewable_type"
+    t.integer "reviewable_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reviewable_type", "reviewable_id"], name: "index_reviews_on_reviewable_type_and_reviewable_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
-  create_table 'roles', force: :cascade do |t|
-    t.string 'title'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "roles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'series', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.datetime 'premier_date'
-    t.string 'language'
-    t.integer 'seasons'
-    t.integer 'duration'
+  create_table "series", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "premier_date"
+    t.string "language"
+    t.integer "seasons"
+    t.integer "duration"
   end
 
-  create_table 'theaters', force: :cascade do |t|
-    t.string 'name'
-    t.text 'address'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "theaters", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'tvs', force: :cascade do |t|
-    t.string 'title'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.datetime 'premire_date'
-    t.string 'language'
-    t.integer 'episode'
-    t.integer 'duration'
+  create_table "tvs", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "premire_date"
+    t.string "language"
+    t.integer "episode"
+    t.integer "duration"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
