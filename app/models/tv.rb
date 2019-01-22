36eb5d86788b8de32b20f6e5ce511dc22749f896
Ankitap123 <ankitapandey141296@gmail.com>
2019-01-22 16:10:33 +0530

    Validations added

--- app/models/tv.rb
+++ app/models/tv.rb
@@ -5,4 +5,9 @@ class Tv < ApplicationRecord
   has_many :reveiews, as: :reviewable
   has_many :genres, as: :genreable
   has_many :awards, as: :awardable
+
+  validates :episode, :duration , numericality: { only_integer: true }
+  validates :title, :premire_date, :episode, presence: true
+  validates :title, format: { with: /\A[a-zA-Z]+\z/}
+
 end
