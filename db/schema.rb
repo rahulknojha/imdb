41127f0affdfb9ac99cd93504cc1b6c1da0697f9
Ankitap123 <ankitapandey141296@gmail.com>
2019-01-22 18:23:40 +0530

    Conflicts resolved

--- db/schema.rb
+++ db/schema.rb
@@ -10,52 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_01_21_075233) do
-  create_table 'active_storage_attachments', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.integer 'record_id', null: false
-    t.integer 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.bigint 'byte_size', null: false
-    t.string 'checksum', null: false
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'awards', force: :cascade do |t|
-    t.string 'award_name'
-    t.string 'category'
-    t.string 'awardable_type'
-    t.integer 'awardable_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[awardable_type awardable_id], name: 'index_awards_on_awardable_type_and_awardable_id'
+  create_table "awards", force: :cascade do |t|
+    t.string "award_name"
+    t.string "category"
+    t.string "awardable_type"
+    t.integer "awardable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["awardable_type", "awardable_id"], name: "index_awards_on_awardable_type_and_awardable_id"
   end
 
-  create_table 'genres', force: :cascade do |t|
-    t.string 'name'
-    t.string 'genreable_type'
-    t.integer 'genreable_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[genreable_type genreable_id], name: 'index_genres_on_genreable_type_and_genreable_id'
-
-  create_table "films_personnels", id: false, force: :cascade do |t|
-    t.integer "film_id", null: false
-    t.integer "personnel_id", null: false
-    t.index ["film_id", "personnel_id"], name: "index_films_personnels_on_film_id_and_personnel_id"
-    t.index ["personnel_id", "film_id"], name: "index_films_personnels_on_personnel_id_and_film_id"
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.string "genreable_type"
+    t.integer "genreable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["genreable_type", "genreable_id"], name: "index_genres_on_genreable_type_and_genreable_id"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -68,19 +62,19 @@ ActiveRecord::Schema.define(version: 2019_01_21_075233) do
     t.datetime "updated_at", null: false
   end
 
-  create_table 'movies_personnels', id: false, force: :cascade do |t|
-    t.integer 'movie_id', null: false
-    t.integer 'personnel_id', null: false
-    t.index [nil], name: 'index_movies_personnels_on_movies_id'
-    t.index [nil], name: 'index_movies_personnels_on_personnels_id'
+  create_table "movies_personnels", id: false, force: :cascade do |t|
+    t.integer "movie_id", null: false
+    t.integer "personnel_id", null: false
+    t.index [nil], name: "index_movies_personnels_on_movies_id"
+    t.index [nil], name: "index_movies_personnels_on_personnels_id"
   end
 
-  create_table 'personnels', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "personnels", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
-  
+
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.string "review"
