468bd6c94307a1b8d6df815339e014c4ba0dd6d4
KaiwalyaPataskar <kaiwalya.pataskar@joshsoftware.com>
2019-01-24 21:05:30 +0530

    Review: Create, Update, Delete using Modal popup

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,23 @@
 class ReviewsController < ApplicationController
-	before_action :find_review, only: %i[show edit destroy]
+	before_action :find_review, only: %i[show edit update destroy]
+	before_action :init_movie, only: [:new]
   def new
   	@review = Review.new
   end
 
   def edit
   	@review = Review.find(params[:id])
+  	#@movie = @review.reviewable_type.constantize.find(@review.reviewable_id)
+  	#render movie_path@movie
   end
 
   def create
   	@review = Review.new(review_params)
   	@review.user_id = current_user.id
   	@review.datetime = DateTime.now.strftime("%d %m %Y | %H:%M")
+  	@movie = @review.reviewable_type.constantize.find(@review.reviewable_id)
     if @review.save
-      redirect_to movies_path, notice: 'New Review has been added successfully.'
+      redirect_to movie_path@movie, notice: 'New Review has been added successfully.'
     else
       flash[:message] = @review.errors.messages
       flash[:errors] = @review.errors.details
@@ -21,11 +25,20 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def update
+  	#@movie = @review.reviewable_type.constantize.find(@review.reviewable_id)
+    if @review.update!(review_params)
+      redirect_to movie_path(@movie)
+    else
+      redirect_to movie_path@movie, notice: 'All fields are mandatory in Appropriate Format!'
+    end
+  end
+
   def destroy
   	if @review.destroy
-      redirect_to movies_path
+      redirect_to movie_path@movie
     else
-      redirect_to movies_path, notice: 'Could not delete the review!'
+      redirect_to movie_path@movie, notice: 'Could not delete the review!'
     end
   rescue ActiveRecord::RecordNotFound => exception
     redirect_to movie_path, alert: exception.message
@@ -33,6 +46,7 @@ class ReviewsController < ApplicationController
 
   def find_review
     @review = Review.find(params[:id])
+    @movie = @review.reviewable_type.constantize.find(@review.reviewable_id)
   rescue ActiveRecord::RecordNotFound => exception
     redirect_to movies_path, alert: exception.message
   end
@@ -41,5 +55,9 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:rating, :review, :reviewable_id, :reviewable_type)
+  end
+
+  def init_movie
+  	@movie = Movie.find(params[:movie_id])
   end
 end
